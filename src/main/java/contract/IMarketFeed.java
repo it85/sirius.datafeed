e84af25b5efa34fbe12b640451ae2f5fe9469569
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 21:15:41 -0500

    added logic to parse apart the json content from YQL

--- src/main/java/contract/IMarketFeed.java
+++ src/main/java/contract/IMarketFeed.java
@@ -1,14 +1,9 @@
 package contract;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
-
-import model.CandleStick;
 
 public interface IMarketFeed {
 	
-	List<CandleStick> staticFeedAsList(String filePath) throws FileNotFoundException;
-	CandleStick getCandleStick() throws IOException;
+	double getQuote() throws IOException;
 
 }
--- src/main/java/model/MarketFeed.java
+++ src/main/java/model/MarketFeed.java
@@ -1,79 +1,93 @@
 package model;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 import contract.IMarketFeed;
-import yahoofinance.Stock;
-import yahoofinance.YahooFinance;
 
 public class MarketFeed implements IMarketFeed{
 
-	String symbol;
+	private String symbol;
 	
 	public MarketFeed(String symbol){
 		this.symbol = symbol;
 	}
 	
-	public CandleStick getCandleStick() throws IOException{
+	public static void main(String[] args){
+		MarketFeed feed = new MarketFeed("SPY");
 		
-		Stock stock = YahooFinance.get(this.symbol);
-		BigDecimal price = stock.getQuote(true).getPrice();
+		try {
+			double d = feed.getQuote();
+			double x = d;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public double getQuote() throws IOException {
 		
+		return Double.parseDouble(this.getLastTradePrice(this.getQuoteAsJson()));
+		
+	}
+	
+	private String getLastTradePrice(String json){
+		
+		String[] array = json.split(",");
+		
+		for(String val : array){
+			
+			if(val.contains("LastTradePriceOnly\":")){
+				String[] priceArray = val.split("\"");
+				return priceArray[3];
+			}			
+		}
 		
 		return null;
 	}
 	
-	public BigDecimal getPrice() throws IOException{
-		Stock stock = YahooFinance.get(this.symbol);
-		return stock.getQuote(true).getPrice();
-	}
-	
-	@Override
-	public List<CandleStick> staticFeedAsList(String filePath) throws FileNotFoundException {
+	private String getQuoteAsJson() throws IOException{
 		
-		if(!fileExists(filePath)){
-			throw new FileNotFoundException();
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		
+		String url = this.getQueryString();
+		
+		HttpGet getRequest = new HttpGet(url);
+		
+		getRequest.addHeader("accept", "application/xml");
+		
+		HttpResponse response = httpClient.execute(getRequest);
+		
+		if (response.getStatusLine().getStatusCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
 		}
 		
-		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = ",";
 		
-		ArrayList<CandleStick> candleStickList = new ArrayList<CandleStick>();
+		String finalString = "";
 		
-		try {
-			
-			br = new BufferedReader(new FileReader(filePath));
-			line = br.readLine();
-				
-			while ((line = br.readLine()) != null) {
-				String[] arr = line.split(cvsSplitBy);
-				CandleStick candle = new CandleStick(arr);
-				candleStickList.add(candle);
-			}
-	
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-				br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}		
-		return candleStickList;
+		BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+		String output;
+		System.out.println("============Output:============");
+		
+		while ((output = br.readLine()) != null) {
+			finalString = finalString + output;
+		}
+		
+		return finalString;
 	}
 	
-	private boolean fileExists(String filePath){
-		return false;
+	private String getQueryString(){
+		String url1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22";
+		String symbol = this.symbol;
+		String url2 = "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+		
+		return url1 + symbol + url2;
 	}
 
 }
